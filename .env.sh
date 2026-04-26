d61bda0e0daf55ccb7542ad0b7632fa8099a6d80
Ranggan Bhattacharyya <142664524+RangganBhattacharyy@users.noreply.github.com>
2026-04-26 21:38:13 +0530

    Add files via upload

---
+++ .env.sh
@@ -0,0 +1,8 @@
+
+#Admin Credentials
+SELLER_EMAIL = "rangganbhattacharyya84@gmail.com"
+SELLER_PASSWORD = "xxxxxxxxxxx"
+
+# MongoDB Setup
+MONGODB_URI = "mongodb+srv://ranggan:<db_password>@cluster0.qbi0fyz.mongodb.net/?appName=Cluster0"
+
